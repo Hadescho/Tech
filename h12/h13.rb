6db20e41d58dc21734108210c1cea0b110302337
Hadescho <Nikolay_Kotzev@abv.bg>
2012-11-14 19:16:21 +0200

    Homework 13 is up but not working

---
+++ h12/h13.rb
@@ -0,0 +1,76 @@
+﻿class Stack
+  @stack
+  @a = 0
+  @b = 0
+	temp = 0
+  def initialize(userInput)
+   @stack = userInput.split " "
+   for i in 0..@stack.size - 1
+		if @stack[i] != "+" and @stack[i] != "-" and @stack[i] != "*" and @stack[i] != "/"
+			@stack = @stack[i].to_i
+		end
+	end
+  end
+	
+  def getstack
+    return @stack
+  end
+  
+  def operation
+	if @stack[0] == "+"
+      puts "v +"
+      @stack[0] = @a+@b
+      @a = 0
+      @b = 0
+    end
+    if @stack[0] == "-"
+      puts "v -"
+      @stack[0]=@a-@b
+      @a = 0
+      @b = 0
+    end
+    if @stack[0] == "*"
+      puts "v *"
+      @stack[0] = @a*@b
+      @a = 0
+      @b = 0
+    end
+    if @stack[0] == "/"
+      puts "v /"
+      @stack[0] = @a/@b
+      @a = 0
+      @b = 0
+    end
+    if @stack[0] == /^[\d]+(\.[\d]+){0,1}$/ 
+     puts " v .match"
+     if a == 0
+       puts "v a==0" 
+       @a = @stack[0].to_i
+	   @stack = @stack[0].to_i
+     else 
+       @b = @stack[0].to_i
+       puts "v b==0" 
+     end
+    end
+	puts @stack
+   @stack = @stack.delete(@stack[0])
+    end
+  def parsestack
+	# puts self.get@stack
+    while @stack.size - 1 > 1
+      self.operation
+	 
+      #puts "tuk ostava bezkraen"
+    end 
+      
+    
+  end
+end
+
+
+userStack = Stack.new(gets.chomp)
+userStack.parsestack()
+puts userStack.getstack
+puts userStack.getstack[0] == 12
+
+
---
+++ h12/polish.rb
@@ -0,0 +1,86 @@
+﻿class PolishStack
+	@stack = Array.new
+	def initialize(userInput)
+		@stack = userInput.split
+	end
+	
+	def getStack
+		return @stack
+	end
+	
+	def pop
+		temp = @stack[0]
+		@stack.delete_at(0)
+		if temp != "+" and temp !="-" and temp != "*" and temp !="/"
+			temp = temp.to_i
+		end
+		return temp
+	end
+	
+	def getStackSize
+		return @stack.size 
+	end
+	
+	def printStack
+		print "["
+		for i in 0..@stack.size - 1
+			print @stack[i]
+			print ","
+		end
+		puts "]"
+	end
+	
+	
+end
+
+class PolishArrays
+	attr_accessor :operators
+	attr_accessor :numbers
+	@operators = Array.new(1,nil)
+	@numbers = Array.new(1,nil)
+	
+	def push(pElement) 
+		if pElement == "+" or pElement == "-" or pElement == "*" or pElement == "/"
+			@operators[@operators.size]=pElement
+			if @operators[0] == nil
+				@operators.delete(nil)
+			end
+		else @numbers[@numbers.size]=pElement
+			if @operators[0] == nil
+				@operators.delete(nil)
+			end
+		end
+	end
+	
+	def pop(arr)
+	#TODO : do a pop method
+	end
+	
+	def print 
+	print "Operators : "
+	print "["
+		for i in 0..@operators.size - 1
+			print @operators[i]
+			print ","
+		end
+		puts "]"
+	print "Numbers : "	
+	print "["
+		for i in 0..@numbers.size - 1
+			print @numbers[i]
+			print ","
+		end
+		puts "]"
+	end
+	
+	def calculate
+	
+	end
+end
+
+userStack = PolishStack.new(gets.chomp)
+userArrays = PolishArrays.new()
+for i in 0..userStack.getStackSize - 1
+	userArrays.push(userStack.pop)
+end
+userArrays.print
